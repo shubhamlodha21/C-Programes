f3e53fa897791bd4a6ec020e14894e5e51a276f1
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-04-23 17:42:08 +0530

    Simple Programes to Prints Jay Ganesh..!

---
+++ Simple1.c
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////
+//Function Name: Prints
+//Input:--
+//Output:--
+//Description: Simple Programes to Prints Jay Ganesh..!
+//Date: 23/04/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<STDIO.h>
+int main()
+{
+	printf("Jay Ganesh...!");
+}
