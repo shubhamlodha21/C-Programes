74738b623fdeea856754bee5888864fb1d0a91bc
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-04-25 00:12:23 +0530

    Simple Program to perform Arithmetic operations

---
+++ Arithmetic1.c
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////
+//Function Name:Main
+//Input:Integer
+//Output:15
+//Description:Simple Programe to perform Arithmetic operations
+//Date: 25/04/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	a=2*3+4%5-3/2+6;
+	printf("%d",a);
+	return 0;
+}
