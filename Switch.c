0704c00e67f072110576456df1072f1a54bc6817
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-04-24 08:15:54 +0530

    Simple Program to Check Switch Case

---
+++ Switch.c
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////
+//Function Name: Prints
+//Input:Integer
+//Output:Integer
+//Description: Simple Program to Check Switch Case
+//Date: 23/04/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	switch(a);
+	printf("Valid");
+}
