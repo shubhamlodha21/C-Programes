7d80cc557ee436c722f4f9e7c1b7d5bac5f88554
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-04-24 08:18:03 +0530

    Simple Program to Used For loop.

---
+++ For.c
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////
+//Function Name: Prints
+//Input:Integer
+//Output:Integer
+//Description: Simple Program to Used For loop.
+//Date: 24/04/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+int main()
+{
+	int i=1;
+	for(i=1;i<32767;i++)
+	printf("%d\n",i);
+}
