b1956270738e46f21403c44dbab5c0fae8c1d230
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-04-25 00:11:23 +0530

    Print Hello World Without Using Semi-colon

---
+++ Hello.c
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////
+//Function Name:Main
+//Input:--
+//Output:Hello World
+//Description:Print Hello World Without Using Semi-colon.
+//Date: 25/04/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+
+int main()
+{
+	if(printf("Hello World"))
+	{}
+return 0;
+}
