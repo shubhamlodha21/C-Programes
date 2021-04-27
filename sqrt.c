6145dac3ecca6fea526ec02bab4f1f9a123a87b8
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-04-27 13:35:36 +0530

    Square-root by using sqrt function.

---
+++ sqrt.c
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////
+//Function Name:main
+//Input:36
+//Output:6
+//Description:Square-root by using sqrt function.
+//Date: 27/04/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+ 
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+   int d,b,a,c;
+   a=36;
+   b=sqrt(a);
+   printf("%d",b);
+}
