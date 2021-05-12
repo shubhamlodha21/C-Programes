c9353ee9424c9345536f94c924abc5d11fa41ded
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-13 00:15:42 +0530

    Accept number from user and display its Power

---
+++ Power.c
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////
+//Function Name:Power
+//Input:4
+//Output:64
+//Description:Accept number from user and display its Power
+//Date: 13/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+# include<stdio.h>
+int Power(int iNo1, int iNo2)
+{
+    int iCnt = 0,iMult = 1;
+    if(iNo1 < 0)
+    {   iNo1 = -iNo1;   }
+    if(iNo2 < 0)
+    {   iNo2 = -iNo2;   }
+    //          1           2               3
+    for(iCnt = 1; iCnt <= iNo2; iCnt++)
+    {
+        iMult = iMult * iNo1;   // 4
+    }
+    return iMult;
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    int iRet = 0;
+    
+    printf("Enter first number\n");
+    scanf("%d",&iValue1);
+    printf("Enter second number\n");
+    scanf("%d",&iValue2);
+    iRet = Power(iValue1,iValue2);
+    printf("Result is : %d\n",iRet);
+    return 0;
+}
