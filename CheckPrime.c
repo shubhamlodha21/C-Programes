700a7d4d784e1508d775518ed8c1cb055ea93257
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-14 10:28:46 +0530

    Accept number from user and check whether it is prime or not

---
+++ CheckPrime.c
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////
+//Function Name:CheckPrime
+//Input:13
+//Output:Prime
+//Description:Accept number from user and check whether it is prime or not
+//Date: 14/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+# include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0;
+    if(iNo < 0)
+    {iNo = -iNo;}
+
+    for(iCnt = 2; iCnt <= iNo/2; iCnt++)
+    {
+        if((iNo % iCnt) == 0)       
+        {
+            break;
+        }
+    }
+    if(iCnt == (iNo/2)+1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet;
+    
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+    bRet = CheckPrime(iValue);
+    if(bRet == true)
+    {
+        printf("%d is prime number\n",iValue);
+    }
+    else
+    {
+        printf("%d is not a prime number\n",iValue);
+    }
+    return 0;
+}
