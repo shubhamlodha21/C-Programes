9063f5c4b8f07b5395978a98c810f7c523d3d596
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-14 10:32:02 +0530

    Accept number from User and Returns Addition of Digits.

---
+++ SumDigits.c
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////
+//Function Name:SumDigits
+//Input:7521
+//Output: 15     (7+5+2+1)
+//Description:Accept number from User and Returns Addition of Digits.
+//Date: 14/05/2021
+//Author: Shubham Lodha
+////////////////////////////////////////////////////////
+#include<stdio.h>
+int SumDigits(int iNo)
+{
+    int iDigit = 0, iSum = 0;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        iSum = iSum + iDigit;
+        iNo = iNo / 10;
+    }
+    return iSum;
+}
+int main()
+{
+    int iValue = 0, iRet = 0;
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+    
+    iRet = SumDigits(iValue);
+    printf("Sum is : %d\n",iRet);
+    
+    return 0;
+}
+
