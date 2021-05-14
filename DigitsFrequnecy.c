3c11e8b502b03e5d782c3afa86c2cd94e291dda6
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-14 10:29:57 +0530

    Accept number from user and Count frequnecy of Perticular frequency.

---
+++ DigitsFrequnecy.c
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////
+//Function Name:DigitFrequnecy
+//Input:75845       5
+//Output:2
+//Description:Accept number from user and Count frequnecy of Perticular frequency.
+//Date: 14/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+int DigitFrequency(int iNo, int i)
+{
+    int iDigit = 0, iCnt = 0;
+    if(iNo < 0)
+    {iNo = -iNo; }
+    if((i < 0)||(i > 9))
+    {
+        printf("Invalid digit\n");
+        return 0;
+    }
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit == i)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0, iRet = 0;
+    printf("Enter number\n");
+    scanf("%d",&iValue1);
+    printf("Enter the digit that you want to seacrch\n");
+    scanf("%d",&iValue2);
+    iRet = DigitFrequency(iValue1,iValue2);
+    printf("Number of given digits are : %d\n",iRet);
+    
+    return 0;
+}
+
