1a78dfbf58ce2cccccf38c34edcedd0c2643160c
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-17 13:24:17 +0530

    Accept number from user and Find its factors.

--- Factors.c
+++ Factors.c
@@ -1,22 +1,23 @@
- ////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
 //Function Name:Factors
 //Input:6
-//Output:1   2   3   4   6
-//Description: Accept number and display factors of that number
-//Date: 13/05/2021
+//Output:1    2    3
+//Description: Accept number from user and Find its factors.
+//Date: 17/05/2021
 //Author: Shubham Lodha
 ///////////////////////////////////////////////////////
 
-# include<stdio.h>
+#include<stdio.h>
+
 void Factors(int iNo)
 {
     int iCnt = 0;
     if(iNo < 0)
     {iNo = -iNo;}
-    
-    for(iCnt = 1; iCnt < iNo; iCnt++)
+  
+    for(iCnt = iNo/2; iCnt >=1; iCnt--)
     {
-        if((iNo % iCnt) == 0)
+        if((iNo % iCnt) == 0)       
         {
             printf("%d\n",iCnt);
         }
@@ -25,11 +26,10 @@ void Factors(int iNo)
 
 int main()
 {
-    int iValue = 0;
-    
+    int iValue1 = 0, iRet = 0;
     printf("Enter number\n");
-    scanf("%d",&iValue);
-    Factors(iValue);
+    scanf("%d",&iValue1);
+    Factors(iValue1);
     
     return 0;
 }
