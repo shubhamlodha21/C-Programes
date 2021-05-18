42a028df523b9113e726cae7fc49fdb3cfaa81f1
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-18 15:54:34 +0530

    print the pattern

---
+++ Pattern2.c
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////
+//Function Name:pattern
+//Input:5
+//Output: A   B   C   D   E
+//Description:print the pattern
+//Date: 18/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+void Pattern(unsigned int iNo)
+{
+    int iCnt = 0;
+    char ch = 'A';
+    printf("\n");
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%c\t",ch);
+        ch++;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    unsigned int iValue = 0;
+    char cValue = '\0';
+    
+    printf("Enter number\n");
+    scanf("%u",&iValue);
+    
+    Pattern(iValue);
+    return 0;
+}
+
