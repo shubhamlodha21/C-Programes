379cfa2e2b24e2b00c318fe418ec6caeb54eb6b5
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-18 15:51:14 +0530

    Find Frequnecy of Digits in between 3 and 7

---
+++ Frequenybetween.c
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////
+//Function Name:Freqency
+//Input:145269
+//Output:3
+//Description:Find Frequnecy of Digits in between 3 and 7
+//Date: 18/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int Frequency(int iNo)
+{
+    int iCnt = 0, iDigit = 0;
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        if((iDigit >= 3) && (iDigit <= 7))
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    int iValue1 = 0, iRet = 0;
+    printf("Enter number\n");
+    scanf("%d",&iValue1);
+    iRet = Frequency(iValue1);
+    
+    printf("Frequency is :%d\n",iRet);
+    return 0;
+}
