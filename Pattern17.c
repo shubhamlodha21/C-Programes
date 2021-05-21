b22024fd91c95cd38eda1bf1011c6d297d692d3c
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-21 09:42:58 +0530

    Print the pattern

---
+++ Pattern17.c
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////
+//Function Name:Pattern
+//Input:Row = 4
+//  Column = 4
+//Output:
+/* 
+            
+            A
+ 
+            A        B
+ 
+            A        B        C
+            
+            A        B        C       D
+ 
+ 
+ */
+//Description:Print the Pattern
+//Date: 21/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+void Pattern(unsigned int iRow, unsigned int iCol)
+{
+    int i = 0, j = 0;
+    char ch = '\0';
+    if(iRow != iCol)
+    {return;}
+    for(i = 1; i<= iRow; i++)
+    {
+        for(j = 1, ch = 'A'; j<= iCol; j++,ch++)
+        {
+            if(i >= j)
+            {
+                printf("%c\t",ch);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    unsigned int iValue1 = 0, iValue2 = 0;
+    
+    printf("Enter number of rows\n");
+    scanf("%u",&iValue1);
+    printf("Enter number of columns\n");
+    scanf("%u",&iValue2);
+    
+    Pattern(iValue1,iValue2);
+    return 0;
+}
