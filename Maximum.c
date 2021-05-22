c3b017bcf2470e99bbdb5b8c76b275e025e1e03e
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-22 12:12:29 +0530

    Accept n Numbers from user and return their maximum number in that

---
+++ Maximum.c
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////
+//Function Name:Maximum
+//Input: (12 28 32 49 50 )
+//Output:50
+//Description:Accept n Numbers from user and return their maximum number in that
+//Date: 22/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+
+int Maximum(int Arr[], int iLength)
+{
+    int i = 0, iMax = 0;
+    if(Arr == NULL)
+    {
+        return -1;
+    }
+    iMax = Arr[0];
+    for(i = 0; i < iLength ;i++)
+    {
+        if(Arr[i] > iMax)
+        {
+            iMax = Arr[i];
+        }
+    }
+    return iMax;
+}
+int main()
+{
+    int *arr = NULL;
+    int i = 0, iSize = 0,iRet = 0;
+    
+    printf("Enter number of elements\n");
+    scanf("%d",&iSize);
+    arr = (int*)malloc(iSize*sizeof(int));
+    printf("Enter the elements\n");
+    for(i = 0; i<iSize; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    iRet = Maximum(arr,iSize);
+    printf("Largest number is: %d\n",iRet);
+    
+    free(arr);
+    return 0;
+}
