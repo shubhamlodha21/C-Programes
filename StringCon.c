bff268b19b8ee74c139edb67c1725489a86efec1
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-29 14:01:32 +0530

    Concatnate the String

---
+++ StringCon.c
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////
+//Function Name:StringCon
+//Input:Shubham Lodha 
+//Output:LodhaShubham
+//Description:Concatnate the String
+//Date: 29/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+
+void strcatX(const char src[], char dest[])
+{
+    if(src == NULL || dest == NULL)
+    {
+        return;
+    }
+    while(*dest != '\0')
+    {
+        dest++;
+    }
+    while(*src!= '\0')
+    {
+        *dest = *src;
+        src++;
+        dest++;
+    }
+    *dest = '\0';
+}
+int main()
+{
+    char arr[20];
+    char brr[20];
+    
+    printf("Enter first string\n");
+    scanf("%[^'\n']s",arr);   //     Demo
+    printf("Enter second string\n");    // Hello
+    scanf(" %[^'\n']s",brr);
+    strcatX(arr,brr);
+    printf("After concat strinng is : %s\n",brr); // HelloDemo
+    
+    return 0;
+}
