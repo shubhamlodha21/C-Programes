d996892a96e5e1046ea6a17e9b92421f3bedfb0e
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-21 10:21:50 +0530

    Bitwise Operations on Given Numbers

---
+++ Bitwise.c
@@ -0,0 +1,27 @@
+/////////////////////////////////////
+//Function Name:Bitwise
+//Input: --
+//Output:--
+//Description:Bitwise Operations on Given Numbers
+//Date:21/06/2021
+//Author:Shubham Lodha
+/////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int a=8,b=9;
+printf("a&b=%d\n",a&b);
+printf("a|b=%d\n",a|b);
+printf("a<<2=%d\n",a<<2);
+printf("b>>3=%d\n",b>>3);
+printf("a^b=%d\n",a^b);
+int x=6,y=5;
+printf("x&y=%d\n",x&y);
+printf("x|y=%d\n",x|y);
+printf("x<<2=%d\n",x<<2);
+printf("y>>3=%d\n",y>>2);
+printf("x^y=%d\n",x^y);
+}
