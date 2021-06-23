508ea61b78a1745e6b6485d8037b9f41c8c747b4
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-24 00:05:36 +0530

    Predict the Output

---
+++ Tp4.c
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output:2 1
+//Description:Predict the Output
+//Date: 24/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+
+
+#include<stdio.h>
+void main()
+{
+int a,b;
+a=(-1,0,1);
+
+b=a++;
+
+printf("%d%d",a,b);
+}
