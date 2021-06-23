db58dc2fe17edad0a0ca71cbae6e7caa80ec8662
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-24 00:05:58 +0530

    Add files via upload

---
+++ Tp5.c
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output:0 -1
+//Description:Predict the Output
+//Date: 24/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+void main()
+{
+int a,b;
+a=-1,0,1;
+
+b=a++;
+
+printf("%d%d",a,b);
+}
