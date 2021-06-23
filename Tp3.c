6271b7e0008b58602dd13acd0c4141e3956067d4
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-24 00:05:17 +0530

    Predict the Output

---
+++ Tp3.c
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output:Inside Default Case
+//Description:Predict the Output
+//Date: 24/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+void main()
+{
+int a=1;
+switch(a)
+{
+   case '1':  printf("case 1");
+                    break;
+   case '2':  printf("case 2");
+                    break;
+   default:  printf("inside default case");
+}
+}
