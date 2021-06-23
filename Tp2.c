fb54f17611caf355dcc0a33796ed603b9f2c281d
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-24 00:05:01 +0530

    Predict the Output

---
+++ Tp2.c
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output:0 1 1
+//Description:Predict the Output
+//Date: 24/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+void main()
+{
+int i;
+for(i=0;i<5;i++)
+{
+        
+        printf("%d ",i&&1);
+        i++;
+}
+}
