018cda0189d7690045adccd42407efb3329e2542
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-25 09:23:14 +0530

    Predict the Output

---
+++ Tp6.c
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output:Enjoying Programming
+//Description:Predict the Output
+//Date: 25/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+void main()
+{
+int i=5;
+if(i!=5)
+ printf("inside if block");
+ printf("enjoying programming");
+printf("out of if");
+}
