c9132cc5060c3716fe431a69f7ae67e51bf5bc16
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-25 09:23:29 +0530

    Predict the Output

---
+++ Tp7.c
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output:Inside if block out of if
+//Description:Predict the Output
+//Date: 25/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+void main()
+{
+int i=1;
+if(i==0);
+printf("inside if block\n");
+printf("out of if\n");
+
+}
