42bde4d62d10c5bf75182b93541fd15cd50dd6bb
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-25 09:23:44 +0530

    Predict the Output

---
+++ Tp8.c
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output:Compilation error
+//Description:Predict the Output
+//Date: 25/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+void main()
+{
+const int a=2;
+a=a+1;
+printf("a=%d",a);
+}
