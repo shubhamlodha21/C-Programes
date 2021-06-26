51059cf02ecd70b75414e87f4115472e32b2fc9f
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-26 13:49:22 +0530

    Predict the Output

---
+++ Tp10.c
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output: inside else block
+//Description:Predict the Output
+//Date: 26/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+void main()
+{
+int i=0;
+if(i++)
+ printf("inside if block");
+else
+ printf("inside else block");
+}
