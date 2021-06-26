82393f4964d14fd12d9a40c5075a66d6bc1691b6
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-26 13:50:20 +0530

    Predict the Output

---
+++ Tp11.c
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////
+//Function Name:Predict
+//Output:Compilation Error
+//Description:Predict the Output
+//Date: 24/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+#include<stdio.h>
+void main()
+ 
+{
+int i=-1;
+for(;i<-2,i++)
+printf("%d",i);
+
+}
