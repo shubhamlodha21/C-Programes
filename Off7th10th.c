84a2d97151dd965e43069a806df4c147fa127dc6
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-26 17:29:17 +0530

    Write a program which accept one number from user and off 7th and 10th bit of that number if it is on

---
+++ Off7th10th.c
@@ -0,0 +1,31 @@
+  
+////////////////////////////////////////////////////////
+//Function Name:ModifiedBits
+//Input:577
+//Output:1
+//Description:Write a program which accept one number from user and off 7th and 10th bit of that number if it is on
+//Date: 26/08/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int ModifiedBits(int iNo)
+{
+    int iMask=0x00000240;
+    int iResult=0;
+
+    iResult=iNo^iMask;
+
+    return iResult;
+}
+
+int main()
+{
+    int iNo=0,iRet=0;
+    printf("Enter a Number:");
+    scanf("%d",&iNo);
+
+    iRet=ModifiedBits(iNo);
+    printf("Modified Number is :%d",iRet);
+}
