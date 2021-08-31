2b2ed8f0d94457ee5ffcfc6f9cb8633548d4bb01
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-31 22:53:05 +0530

    Write a program which accept one number and position from user and on that bit. Return modified number

---
+++ LB293.c
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////
+//Function Name:ChkBit
+//Input:10  3
+//Output:14
+//Description:Write a program which accept one number and position from user and on that bit. Return modified number
+//Date: 31/08/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int ChkBit(int iNo,int iPos)
+{
+    if((iPos>=33) || (iPos<0))
+    {
+        return -1;
+    }
+    unsigned int iMask=0X00000001;
+    int iResult=0;
+
+    iMask=iMask<<(iPos-1);
+
+    iResult=iNo|iMask;
+    
+    return iResult;
+}
+
+int main()
+{
+    int iNo=0,iPos=0,iRet=0;
+
+    printf("Enter a Number:");
+    scanf("%d",&iNo);
+
+    printf("Enter a  Position:");
+    scanf("%d",&iPos);
+
+    iRet=ChkBit(iNo,iPos);
+    if(iRet==-1)
+    {
+        printf("Invalid Position..!\n");
+    }
+    else
+    {
+        printf("Modified Number is %d",iRet);
+    }
+
+}
