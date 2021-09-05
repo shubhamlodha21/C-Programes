7ec94ae02ed1ad14ca8f2e7eba8bfb8048a9a69a
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-09-05 22:34:56 +0530

    Print the pattern

--- LB104.c
+++ LB104.c
@@ -1,9 +1,9 @@
 ////////////////////////////////////////////////////////
 //Function Name:Pattern
-//Input:4
-//Output:# 1 * # 2 * # 3 * # 4 *
-//Description:Display the Pattern
-//Date: 27/07/2021
+//Input:5
+//Output:#       1       *       #       2       *       #       3       *       #       4       *       #       5       *
+//Description:Print the Pattern
+//Date: 05/09/2021
 //Author: Shubham Lodha
 //////////////////////////////////////////////////////
 
