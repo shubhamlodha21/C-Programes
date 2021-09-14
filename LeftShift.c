702ab2055a630d27ed5ea2a16128eaf23b4e90a0
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-09-14 22:36:27 +0530

    Implementations of left-shift operator

---
+++ LeftShift.c
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////
+//Function Name:--
+//Input:5
+//Output:20
+//Description:Implementations of left-shift operator
+//Date: 14/09/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+int main()
+{
+    int iNo=5;
+    iNo=iNo<<2;
+    printf("Value of 5 after left shift is: %d",iNo);
+}
